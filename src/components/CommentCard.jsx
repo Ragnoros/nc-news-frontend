89fcf7ffc4c70605e2744fd30e18f65105fe8592
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-09-18 10:36:13 +0100

    Task 6 Completed

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,13 @@
+export function CommentCard({ comment }) {
+  console.log(comment);
+
+  if (comment) {
+    return (
+      <div className="commentCard">
+        <h4>{comment.author}</h4>
+        <p>{comment.body}</p>
+        <p>{comment.created_at}</p>
+      </div>
+    );
+  }
+}
