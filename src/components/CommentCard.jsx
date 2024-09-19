a8291454ef40105e91183135fec4e5cc3655fca8
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-09-19 20:51:36 +0100

    Task 9 Completed

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,9 +1,17 @@
-export function CommentCard({ comment }) {
+import { deleteComment } from "./api";
+
+export function CommentCard({ comment, user }) {
+  function handleClick() {
+    deleteComment(comment.comment_id);
+  }
   return (
     <div className="commentCard">
       <h4>{comment.author}</h4>
       <p>{comment.body}</p>
       <p>{comment.created_at}</p>
+      {user === comment.author ? (
+        <button onClick={handleClick}>Delete Comment</button>
+      ) : null}
     </div>
   );
 }
